849fc53d6f34bf299db768c4aa42dd09ca9b6fc9
IuriRaRedu <iuri.ramos.reducino@gmail.com>
2020-06-15 23:21:38 -0300

    First Commit

--- src/test/java/Selenium.java
+++ src/test/java/Selenium.java
@@ -4,37 +4,39 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.Random;
 
 public class Selenium {
 
     // Criar instancia do driver do chrome
-    private WebDriver driver;
+    private static WebDriver driver;
+    private WebDriverWait wait = new WebDriverWait(driver,100);
+
 
     @BeforeAll
-    public void setup() {
+    public static void setup() {
         // Setar as propriedades  do ChromeDrive
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 
         driver = new ChromeDriver();
-
         // Abrir Browser e acessar URL
-        driver.get("https://automacaocombatista.herokuapp.com/");
-
-
-        driver.findElement(By.xpath("//a[text()='Começar Automação Web']")).click();
+        driver.get("https://automacaocombatista.herokuapp.com");
 
         // Maximinizando o Browser
         driver.manage().window().maximize();
+
+
+        driver.findElement(By.xpath("//a[text()='Começar Automação Web']")).click();
     }
 
     @Test
     public void criarUsuario(){
         Random random = new Random();
         String email = "iuri"+random.nextInt(1000)+"@gmail.com";
-
 
         driver.findElement(By.xpath("//a[text()='Formulário']")).click();
         driver.findElement(By.xpath("//a[text()='Criar Usuários']")).click();
@@ -47,7 +49,7 @@ public class Selenium {
         driver.findElement(By.id("user_university")).sendKeys("FATEC Franco da Rocha");
         driver.findElement(By.id("user_profile")).sendKeys("Agente de Atendimento");
         driver.findElement(By.id("user_gender")).sendKeys("Masculino");
-        driver.findElement(By.id("user_age")).sendKeys("27");
+        driver.findElement(By.id("user_age")).sendKeys(String.valueOf(random.nextInt(30)));
 
         // Clicando no Botão
         driver.findElement(By.name("commit")).click();
@@ -64,23 +66,22 @@ public class Selenium {
     @Test
     public void radioCheckBox(){
         driver.findElement(By.xpath("//a[text()='Busca de elementos']")).click();
-        driver.findElement(By.xpath("//a[text()='Radio e Checkbox']")).click();
+        WebElement link = driver.findElement(By.xpath("//a[text()='Radio e Checkbox']"));
+        wait.until(ExpectedConditions.visibilityOf(link));
+        link.click();
 
-
-
-        driver.findElement(By.xpath("//input[@id='red']")).click();
-        driver.findElement(By.xpath("//input[@id='blue']")).click();
-        driver.findElement(By.xpath("//input[@id='yellow']")).click();
-        driver.findElement(By.xpath("//input[@id='green']")).click();
-        driver.findElement(By.xpath("//input[@id='purple']")).click();
-        driver.findElement(By.xpath("//input[@id='grey']")).click();
-        driver.findElement(By.xpath("//input[@id='black']")).click();
-        driver.findElement(By.xpath("//input[@id='white']")).click();
-
+        driver.findElement(By.xpath("//input[@id='red']/following-sibling:: label")).click();
+        driver.findElement(By.xpath("//input[@id='blue']/following-sibling:: label")).click();
+        driver.findElement(By.xpath("//input[@id='yellow']/following-sibling:: label")).click();
+        driver.findElement(By.xpath("//input[@id='green']/following-sibling:: label")).click();
+        driver.findElement(By.xpath("//input[@id='purple']/following-sibling:: label")).click();
+        driver.findElement(By.xpath("//input[@id='grey']/following-sibling:: label")).click();
+        driver.findElement(By.xpath("//input[@id='black']/following-sibling:: label")).click();
+        driver.findElement(By.xpath("//input[@id='white']/following-sibling:: label")).click();
     }
 
     @AfterAll
-    public void fecharBrowser(){
+    public static void fecharBrowser(){
         // Fechando o navegador
         driver.quit();
     }
